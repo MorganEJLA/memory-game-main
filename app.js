41982090a87abfe9576c2a994d6178aeda44cc3d
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2023-11-08 17:12:04 -0500

    updated js

--- app.js
+++ app.js
@@ -1,127 +1,184 @@
-class MixOrMatch {
-    constructor(totalTime, cards) {
-        this.cardsArray = cards;
-        this.totalTime = totalTime;
-        this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remaining');
-        this.ticker = document.getElementById('flips');
-        // this.audioController = new AudioController();
-    }
-    startGame() {
-        this.cardToCheck = null;
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime;
-        this.matchedCards = [];
-        this.busy = true;
-        setTimeout(() => {
-           
-            this.shuffleCards();
-            this.countDown = this.startCountDown();
-            this.busy = false;
-        }, 500);
-        this.hideCards();
-        this.timer.innerText = this.timeRemaining;
-        this.ticker.innerText = this.totalClicks;
-    }
-    hideCards() {
-        this.cardsArray.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.remove('matched');
-        });
-    }
-    flipCard(card) {
-        if(this.canFlipCard(card)) {
-            // this.audioController.flip();
-            this.totalClicks++;
-            this.ticker.innerText = this.totalClicks;
-            card.classList.add('visible');
+const gridContainer = document.querySelector(".grid-container");
+let cards = [];
+let firstCard, secondCard;
+let lockBoard = false;
+let score = 0;
+let timerInterval;
+let timeInSeconds = 0;
+const maxScore = 10000;
+const minTime = 120;
+let highestScore = localStorage.getItem("highestScore") || 0;
 
-            if(this.cardToCheck)
-                this.checkForCardMatch(card);
-            else
-                this.cardToCheck = card;
-        }
-    }
-    checkForCardMatch(card) {
-        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
-            this.cardMatch(card, this.cardToCheck);
-        else 
-            this.cardMisMatch(card, this.cardToCheck);
-
-        this.cardToCheck = null;
-    }
-    cardMatch(card1, card2) {
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-       
-        if(this.matchedCards.length === this.cardsArray.length)
-            this.victory(); 
-    }
-    cardMisMatch(card1, card2) {
-        this.busy = true;
-        setTimeout(() => {
-            card1.classList.remove('visible');
-            card2.classList.remove('visible');
-            this.busy = false;
-        }, 1000);
-    }
-    getCardType(card) {
-        return card.getElementsByClassName('card-value')[0].src;
-    }
-    startCountDown() {
-        return setInterval(() => {
-            this.timeRemaining--;
-            this.timer.innerText = this.timeRemaining;
-            if(this.timeRemaining === 0)
-                this.gameOver();
-        }, 1000);
-    }
-    gameOver() {
-        clearInterval(this.countDown);
-        // this.audioController.gameOver();
-        document.getElementById('game-over-text').classList.add('visible');
-    }
-    victory() {
-        clearInterval(this.countDown);
-        // this.audioController.victory();
-        document.getElementById('victory-text').classList.add('visible');
-    }
-
-    shuffleCards() {
-        for(let i = this.cardsArray.length - 1; i > 0; i--) {
-            let randIndex = Math.floor(Math.random() * (i+1));
-            this.cardsArray[randIndex].style.order = i;
-            this.cardsArray[i].style.order = randIndex;
-        }
-    }
-
-    canFlipCard(card) {
-        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
-    }
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timeInSeconds++;
+    updateTimerDisplay();
+  }, 1000);
 }
 
-function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(100, cards);
-
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-            game.startGame();
-        });
-    });
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
-    });
+function stopTimer() {
+  clearInterval(timerInterval);
 }
 
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+function updateTimerDisplay() {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+
+  const formattedTime = `${padZero(minutes)}:${padZero(seconds)}`;
+  document.querySelector(".timer").textContent = formattedTime;
 }
+
+function padZero(number) {
+  return number.toString().padStart(2, "0");
+}
+
+document.querySelector(".score").textContent = score;
+
+fetch("/data/cards.json")
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    generateCards();
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
+  });
+
+function shuffleCards() {
+  let currentIndex = cards.length,
+    randomIndex,
+    temporaryValue;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
+}
+
+function generateCards() {
+  for (let card of cards) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.setAttribute("data-name", card.name);
+    cardElement.innerHTML = `
+      <div class="front">
+        <img class="front-image" src=${card.image} />
+      </div>
+      <div class="back"></div>
+    `;
+    gridContainer.appendChild(cardElement);
+    cardElement.addEventListener("click", flipCard);
+  }
+}
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flipped");
+  this.querySelector(".front").classList.add("show");
+
+  if (!firstCard) {
+    firstCard = this;
+    if (!timerInterval) {
+      startTimer();
+    }
+  } else {
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
+  }
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+  if (isMatch) {
+    disableCards();
+    score++;
+    document.querySelector(".score").textContent = score;
+    if (score === cards.length / 2) {
+      gameWon();
+    } else {
+      resetBoard();
+    }
+  } else {
+    setTimeout(() => {
+      unflipCards();
+    }, 500);
+  }
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    firstCard.querySelector(".front").classList.remove("show");
+    secondCard.querySelector(".front").classList.remove("show");
+    resetBoard();
+  }, 500);
+}
+
+function calculateScore(timeInSeconds) {
+  return Math.max(0, maxScore - (timeInSeconds - minTime) * 100);
+}
+
+function displayHighestScore() {
+  const highestScoreSpan = document.querySelector(".highest-score");
+  highestScoreSpan.textContent = highestScore || 0;
+}
+
+function resetBoard() {
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
+}
+
+function gameWon() {
+  stopTimer();
+
+  const timeTaken = timeInSeconds;
+  const finalScore = calculateScore(timeTaken);
+
+  document.querySelector(".score").textContent = finalScore;
+
+  if (finalScore > (highestScore || 0)) {
+    highestScore = finalScore;
+    localStorage.setItem("highestScore", highestScore);
+  }
+  displayHighestScore();
+}
+
+function restart() {
+  resetBoard();
+  shuffleCards();
+  score = 0;
+  document.querySelector(".score").textContent = score;
+
+  // Stop the timer if it's running
+  if (timerInterval) {
+    stopTimer();
+  }
+
+  resetTimer();
+  startTimer();
+
+  gridContainer.innerHTML = "";
+  generateCards();
+}
+function resetTimer() {
+  stopTimer();
+  timeInSeconds = 0;
+  updateTimerDisplay();
+}
+// Initialize the highest score display
+displayHighestScore();
